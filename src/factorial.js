d673c0569f34d9f0c15f01351d4e45ca99d38d21
rauf-babayev <rauf.rb.business@gmail.com>
2020-03-14 12:56:26 +0400

    factorial task done

---
+++ src/factorial.js
@@ -0,0 +1,5 @@
+const getFactorial = (number) => {
+    if (!number || number < 0) return 'Wrong input!';
+    return (number === 1) ? 1 : number * getFactorial(number - 1);
+};
+module.exports = getFactorial;
