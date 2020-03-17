ff655091a25525862fab091d268b852a57bf459c
rauf-babayev <rauf.rb.business@gmail.com>
2020-03-17 18:57:34 +0400

    toArray method done

---
+++ src/Tree.js
@@ -0,0 +1,53 @@
+function Node (value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+};
+
+function BST () {
+    this.root = null;
+};
+
+BST.prototype.addNode = function (value) {
+    if (!value && value !== 0) {
+        return '';
+    }
+
+    this.root = add(this.root, value);
+    function add(node, value) {
+        if (!node) {
+            return new Node(value);
+        } else if (value < node.value) {
+            node.left = add(node.left, value);
+        } else {
+            node.right = add(node.right, value);
+        }
+
+        return node;
+    }
+};
+
+BST.prototype.init = function (array) {
+    for (let i = 0; i < array.length; i++) {
+        this.addNode(array[i]);
+    }
+};
+
+BST.prototype.clearBST = function () {
+    this.root = null;
+};
+
+BST.prototype.toArray = function() {
+    return toArrayNode(this.root);
+};
+
+function toArrayNode(node) {
+    if (!node) {
+        return [];
+    }
+
+    const left = toArrayNode(node.left);
+    const right = toArrayNode(node.right);
+
+    return [...left, node.value, ...right];
+};
