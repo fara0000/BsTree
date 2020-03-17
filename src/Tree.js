d3d88495c37fd100ce59d0f8f6b1f154d6c65fb3
rauf-babayev <61136400+rauf-babayev@users.noreply.github.com>
2020-03-17 22:50:29 +0400

    Update Tree.js

--- src/Tree.js
+++ src/Tree.js
@@ -8,51 +8,9 @@ module.exports = Node;
 
 function BST () {
     this.root = null;
-    this.size = 0;
 };
 
 module.exports = BST;
-
-BST.prototype.addNode = function(value) {
-    this.size++;
-    const node = this.root;
-
-    if(node === null) {
-        this.root = new Node(value);
-        return;
-    } else {
-        const searchTree = function (node) {
-            if (value < node.value) {
-                if (node.left === null) {
-                    node.left = new Node (value);
-                } else if ( node.left !== null) {
-                    return searchTree(node.left);
-                }
-            } else if (value > node.value) {
-                if (node. right === null) {
-                    node.right = new Node (value);
-                } else if (node.right !== null) {
-                    return searchTree(node.right);
-                }
-            } else {
-                return null;
-            }
-        }
-
-        return searchTree(node);
-    }
-};
-
-BST.prototype.clear = function (node) {
-    this.root = 0;
-    this.size = 0;
-};
-
-BST.prototype.init = function (array) {
-    for(i = 0; i < array.length; i++) {
-        this.addNode(array[i]);
-    }
-};
 
 BST.prototype.findNode = function (value) {
     let currentNode = this.root;
@@ -68,14 +26,6 @@ BST.prototype.findNode = function (value) {
     }
     return false;
 }; 
-
-BST.prototype.getSize = function() {
-    return this.size;
-};
-
-function BST () {
-    this.root = null;
-};
 
 BST.prototype.addNode = function (value) {
     if (!value && value !== 0) {
