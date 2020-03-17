d6a7ecd6f6261f6fc2c8e7a17c8f3640ba57846c
SevincMammadova <sevincmammadova609@gmail.com>
2020-03-17 16:57:35 +0400

    method addNode done

--- src/Tree.js
+++ src/Tree.js
@@ -4,112 +4,33 @@ function Node(value) {
     this.right = null;
 };
 
-module.exports = Node;
-
 function BST () {
     this.root = null;
     this.size = 0;
 };
 
-module.exports = BST;
-
-BST.prototype.addNode = function(value) {
+BST.prototype.addNode = function (value) {
     this.size++;
-    const node = this.root;
 
-    if(node === null) {
-        this.root = new Node(value);
+    if(!value && value !== 0) {
         return;
-    } else {
-        const searchTree = function (node) {
-            if (value < node.value) {
-                if (node.left === null) {
-                    node.left = new Node (value);
-                } else if ( node.left !== null) {
-                    return searchTree(node.left);
-                }
-            } else if (value > node.value) {
-                if (node. right === null) {
-                    node.right = new Node (value);
-                } else if (node.right !== null) {
-                    return searchTree(node.right);
-                }
-            } else {
-                return null;
-            }
-        }
+    };
 
-        return searchTree(node);
-    }
-};
+    this.root = add(this.root, value);
 
-BST.prototype.clear = function (node) {
-    this.root = 0;
-    this.size = 0;
-};
-
-BST.prototype.init = function (array) {
-    for(i = 0; i < array.length; i++) {
-        this.addNode(array[i]);
-    }
-};
-
-BST.prototype.findNode = function (value) {
-    let currentNode = this.root;
-    while (currentNode) {
-        if (value === currentNode.value) {
-            return true;
-        }
-        if (value < currentNode.value) {
-            currentNode = currentNode.left
+    function add(rootElm, value) {
+        if (!rootElm) {
+            return new Node(value);
+        } else if (value < rootElm.value) {
+            rootElm.left = add(rootElm.left, value)
         } else {
-            currentNode = currentNode.right;
+            rootElm.right = add(rootElm.right, value)
         }
+
+        return rootElm;
     }
-    return false;
-}; 
+}
 
-BST.prototype.getSize = function() {
-    return this.size;
-};
+module.exports = Node;
 
-// BST.prototype.Delete = function(value) {
-//     let removeNode = function(node, value) {
-//     if (node == null) {
-//     return null;
-//     }
-//     if (value == node.value) {
-//     //if node has no children
-//     if (node.left == null && node.right == null) {
-//     return null;
-//     }
-//     //if node has no left child
-//     if (node.left == null) {
-//     return node.right;
-//     }
-//     //if node has no right child
-//     if (node.right == null) {
-//     return node.left;
-//     }
-//     //node has two children
-//     var tempNode = node.right;
-//     while (tempNode.left !== null) {
-//     tempNode = tempNode.left;
-//     }
-//     node.value = tempNode.value;
-//     node.right = removeNode (node.right, tempNode.value);
-//     return node;
-//     } else if (value < node.value) {
-//     node.left = removeNode (node.left, value);
-//     return node;
-//     } else {
-//     node.right = removeNode (node.right, value);
-//     return node;
-//     }
-//     }
-//     this.root = removeNode(this.root, value);
-//     };
-
-
-
-
+module.exports = BST;
