6703c5b04ee13f4e8335b29ca60e6504a8d0bfd7
SevincMammadova <61136381+SevincMammadova@users.noreply.github.com>
2020-03-17 22:45:27 +0400

    method getSize and some test are done (#3)
    Co-authored-by: rauf-babayev <61136400+rauf-babayev@users.noreply.github.com>

--- src/Tree.js
+++ src/Tree.js
@@ -4,6 +4,75 @@ function Node (value) {
     this.right = null;
 };
 
+module.exports = Node;
+
+function BST () {
+    this.root = null;
+    this.size = 0;
+};
+
+module.exports = BST;
+
+BST.prototype.addNode = function(value) {
+    this.size++;
+    const node = this.root;
+
+    if(node === null) {
+        this.root = new Node(value);
+        return;
+    } else {
+        const searchTree = function (node) {
+            if (value < node.value) {
+                if (node.left === null) {
+                    node.left = new Node (value);
+                } else if ( node.left !== null) {
+                    return searchTree(node.left);
+                }
+            } else if (value > node.value) {
+                if (node. right === null) {
+                    node.right = new Node (value);
+                } else if (node.right !== null) {
+                    return searchTree(node.right);
+                }
+            } else {
+                return null;
+            }
+        }
+
+        return searchTree(node);
+    }
+};
+
+BST.prototype.clear = function (node) {
+    this.root = 0;
+    this.size = 0;
+};
+
+BST.prototype.init = function (array) {
+    for(i = 0; i < array.length; i++) {
+        this.addNode(array[i]);
+    }
+};
+
+BST.prototype.findNode = function (value) {
+    let currentNode = this.root;
+    while (currentNode) {
+        if (value === currentNode.value) {
+            return true;
+        }
+        if (value < currentNode.value) {
+            currentNode = currentNode.left
+        } else {
+            currentNode = currentNode.right;
+        }
+    }
+    return false;
+}; 
+
+BST.prototype.getSize = function() {
+    return this.size;
+};
+
 function BST () {
     this.root = null;
 };
--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,104 +1,324 @@
-// const { assert } = require ('chai');
-// const getFactorial = require ('../Factorial');
-// describe('getFactorial', () => {
-//     it('should be defined', () => {
-//         assert.isDefined(getFactorial, 'function has been defined')
-//     });
+const { assert } = require ('chai');
+const Node = require ('../Tree');
+const BST = require ('../Tree');
 
-//     it('should return Warning (undefined)', () => {
-//         const num = undefined;
-//         const expected = 'Wrong input!';
+describe('Binary Search Tree tests', () => {
 
-//         const actual = getFactorial(num);
+    describe('function Node', () => {
+        it('function Node defined',() => {
+            assert.isDefined(Node);
+        });
+    });
 
-//         assert.deepEqual(actual, expected);
-//     });
+    describe('function BST', () => {
+        it('function BST defined', () => {
+            assert.isDefined(BST);
+        });
+    });
 
-//     it('should return Warning (null)', () => {
-//         const num = null;
-//         const expected = 'Wrong input!';
+    describe('BST method init', () => {
+        it('method init defined', () => {
+            let test = new BST();
 
-//         const actual = getFactorial(num);
+            assert.isDefined(test.init);
+        });
 
-//         assert.deepEqual(actual, expected);
-//     });
+        it('method init works correctly', () => {
+            let test = new BST();
+            const array = [5, 6, 8];
+            const expectedSize = 3;
+            
+            test.init(array);
 
-//     it('should return Warning (")', () => {
-//         const num = ''; 
-//         const expected = 'Wrong input!';
+            assert.deepEqual(test.getSize(), expectedSize);
+        });
+    });
 
-//         const actual = getFactorial(num);
+    describe('BST method addNode', () => {
+        it('addNode defined',() => {
+            let test = new BST();
 
-//         assert.deepEqual(actual, expected);
-//     });
+            assert.isDefined(BST.prototype.addNode);
+        });
 
-//     it('should return Warning (-2)', () => {
-//         const num = -2; 
-//         const expected = 'Wrong input!';
+        it('method addNode works correctly in empty tree', () => {
+            let test = new BST();
+            let array = [];
+            test.init(array);
+            const expected = 1;
+            const expectedSize = 1;
+            
+            const actual = test.addNode((1));
 
-//         const actual = getFactorial(num);
+            assert.deepEqual(test.getSize(), expectedSize);
+        });
 
-//         assert.deepEqual(actual, expected);
-//     });
+        it('method addNode works correctly when tree have 1 node', () => {
+            let test = new BST();
+            let array = [1];
+            test.init(array);
+            const expectedSize = 2;
+            
+            const actual = test.addNode(2);
 
-//     it('should return Warning (-7)', () => {
-//         const num = -7; 
-//         const expected = 'Wrong input!';
+            assert.deepEqual(test.getSize(), expectedSize);
+        });
 
-//         const actual = getFactorial(num);
+        it('method addNode works correctly when tree have 2 nodes', () => {
+            let test = new BST();
+            let array = [1,2];
+            test.init(array);
+            const expectedSize = 3;
+            
+            const actual = test.addNode((5));
 
-//         assert.deepEqual(actual, expected);
-//     });
+            assert.deepEqual(test.getSize(), expectedSize);
+        });
 
-//     it('should return Factrorial (3)', () => {
-//         const num = 3; 
-//         const expected = 6;
+        it('method addNode works correctly when tree have many nodes', () => {
+            let test = new BST();
+            let array = [10,2,6,4,8,9];
+            test.init(array);
+            const expectedSize = 7;
+            
+            const actual = test.addNode((5));
 
-//         const actual = getFactorial(num);
+            assert.deepEqual(test.getSize(), expectedSize);
+        });
+    });
 
-//         assert.deepEqual(actual, expected);
-//     });
+    describe('BST method clear', () => {
+        it('method clear defined', () => {
+            let test = new BST();
+            assert.isDefined(test.clear);
+        });
 
-//     it('should return Factorial(0)', () => {
-//         const num = 1;
-//         const expected = 1;
+        it('method clear works correctly when empty tree', () => {
+            let test = new BST();
+            const array = [];
+            test.init(array);
+            const expectedSize = 0;
 
-//         const actual = getFactorial(num);
+            test.clear();
 
-//         assert.deepEqual(actual, expected);
-//     });
+            assert.deepEqual(test.getSize(), expectedSize);
+        });
 
-//     it('should return Factrorial (12)', () => {
-//         const num = 12; 
-//         const expected = 479001600;
+        it('method clear works correctly when tree have 1 node', () => {
+            let test = new BST();
+            const array = [1];
+            test.init(array);
+            const expectedSize = 0;
 
-//         const actual = getFactorial(num);
+            test.clear();
 
-//         assert.deepEqual(actual, expected);
-//     });
+            assert.deepEqual(test.getSize(), expectedSize);
+        });
 
-//     it('should return Factrorial (5)', () => {
-//         const num = 5; 
-//         const expected = 120;
+        it('method clear works correctly when tree have 2 nodes', () => {
+            let test = new BST();
+            const array = [1, 2];
+            test.init(array);
+            const expectedSize = 0;
 
-//         const actual = getFactorial(num);
+            test.clear();
 
-//         assert.deepEqual(actual, expected);
-//     });
+            assert.deepEqual(test.getSize(), expectedSize);
+        });
 
-//     it('should return Factrorial (6)', () => {
-//         const num = 6; 
-//         const expected = 720;
+        it('method clear works correctly when tree have 3 nodes', () => {
+            let test = new BST();
+            const array = [4, 2, 6];
+            test.init(array);
+            const expectedSize = 0;
 
-//         const actual = getFactorial(num);
+            test.clear();
 
-//         assert.deepEqual(actual, expected);
-//     });
-// });
+            assert.deepEqual(test.getSize(), expectedSize);
+        });
 
+        it('method clear works correctly when tree have many nodes', () => {
+            let test = new BST();
+            const array = [10, 5, 6, 11, 45];
+            test.init(array);
+            const expectedSize = 0;
 
+            test.clear();
 
+            assert.deepEqual(test.getSize(), expectedSize);
+        });
+    });
 
+    describe('BST metod getSize', () => {
+        it('method getSize defined', () => {
+            let test = new BST();
+            
+            assert.isDefined(test.getSize);
+        });
+
+        it('method getSize works correctly when empty tree', () => {
+            let test = new BST();
+            const array = [];
+            test.init(array);
+            const expectedSize = 0;
+
+            const actual = test.getSize();
+
+            assert.deepEqual(actual, expectedSize);
+        });
+
+        it('method getSize works correctly when tree have 1 node', () => {
+            let test = new BST();
+            const array = [5];
+            test.init(array);
+            const expectedSize = 1;
+
+            const actual = test.getSize();
+
+            assert.deepEqual(actual, expectedSize);
+        });
+
+        it('method getSize works correctly when tree have 2 nodes', () => {
+            let test = new BST();
+            const array = [2, 5];
+            test.init(array);
+            const expectedSize = 2;
+
+            const actual = test.getSize();
+
+            assert.deepEqual(actual, expectedSize);
+        });
+
+        it('method getSize works correctly when tree have 3 nodes', () => {
+            let test = new BST();
+            const array = [5, 4, 10];
+            test.init(array);
+            const expectedSize = 3;
+
+            const actual = test.getSize();
+
+            assert.deepEqual(actual, expectedSize);
+        });
+
+        it('method getSize works correctly when tree have many nodes', () => {
+            let test = new BST();
+            const array = [15, 23, 5, 6, 1];
+            test.init(array);
+            const expectedSize = 5;
+
+            const actual = test.getSize();
+
+            assert.deepEqual(actual, expectedSize);
+        });
+    });
+
+    describe('BST method findnode', () => {
+        it('method findNode defined', () => {
+            let test = new BST();
+
+            assert.isDefined(test.findNode);
+        });
+
+        it('method findNode works correctly when empty tree', () => {
+            let test = new BST();
+            const array = [];
+            const expected = false;
+            test.init(array);
+
+            const actual = test.findNode(5);
+
+            assert.deepEqual(actual, expected);
+        });
+
+        it('method findNode works correctly when tree have 1 node', () => {
+            let test = new BST();
+            const array = [1];
+            const expected = true;
+            test.init(array);
+
+            const actual = test.findNode(1);
+
+            assert.deepEqual(actual, expected);
+        });
+
+        it('method findNode works correctly when tree have 1 node, result = false', () => {
+            let test = new BST();
+            const array = [1];
+            const expected = false;
+            test.init(array);
+
+            const actual = test.findNode(5);
+
+            assert.deepEqual(actual, expected);
+        });
+
+        it('method findNode works correctly when tree have 2 nodes', () => {
+            let test = new BST();
+            const array = [1, 8];
+            const expected = true;
+            test.init(array);
+
+            const actual = test.findNode(1);
+
+            assert.deepEqual(actual, expected);
+        });
+
+        it('method findNode works correctly when tree have 2 nodes, result = false', () => {
+            let test = new BST();
+            const array = [1, 2];
+            const expected = false;
+            test.init(array);
+
+            const actual = test.findNode(5);
+
+            assert.deepEqual(actual, expected);
+        });
+
+        it('method findNode works correctly when tree have 3 nodes', () => {
+            let test = new BST();
+            const array = [5, 8, 4];
+            const expected = true;
+            test.init(array);
+
+            const actual = test.findNode(5);
+
+            assert.deepEqual(actual, expected);
+        });
+
+        it('method findNode works correctly when tree have 3 nodes, result = false', () => {
+            let test = new BST();
+            const array = [4, 5, 89];
+            const expected = false;
+            test.init(array);
+
+            const actual = test.findNode(8);
+
+            assert.deepEqual(actual, expected);
+        });
+
+        it('method findNode works correctly when tree have many nodes', () => {
+            let test = new BST();
+            const array = [5, 8, 4, 89, 56];
+            const expected = true;
+            test.init(array);
+
+            const actual = test.findNode(56);
+
+            assert.deepEqual(actual, expected);
+        });
+
+        it('method findNode works correctly when tree have many nodes, result = false', () => {
+            let test = new BST();
+            const array = [4, 5, 89, 56, 44, 2];
+            const expected = false;
+            test.init(array);
+
+            const actual = test.findNode(8);
+
+            assert.deepEqual(actual, expected);
+        });
+    });
+})
 
 const makeCounter = require ('../factorial');
 
